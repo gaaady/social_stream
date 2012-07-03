a4b8853fc58be7c140127ce43ea2b473ba0ec556
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-07-03 10:13:25 +0200

    Revert "Add notifications upon draft (warning, enables update activities)"
    This reverts commit d56230b20ef440025a4d96c0f310f51496d8392b.

--- base/db/migrate/20120701114809_add_notified_flag_to_activity.rb
+++
@@ -1,9 +0,0 @@
-class AddNotifiedFlagToActivity < ActiveRecord::Migration
-  def up
-    add_column :activities, :notified, :boolean, :default => false
-  end
-
-  def down
-    remove_column :activities, :notified
-  end
-end
