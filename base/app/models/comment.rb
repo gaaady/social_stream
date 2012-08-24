b9ac1f95af538ea153738319fda31f9e9ae81614
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-08-24 16:01:13 +0200

    Oooops, check for nil activity_object in comment model

--- base/app/models/comment.rb
+++ base/app/models/comment.rb
@@ -35,7 +35,7 @@ class Comment < ActiveRecord::Base
   def increment_comment_count_and_touch
     return if self.post_activity.parent.blank?
  
-    self.post_activity.parent.direct_activity_object.increment!(:comment_count) 
+    self.post_activity.parent.direct_activity_object.increment!(:comment_count) unless self.post_activity.parent.direct_activity_object.blank?
     self.post_activity.parent.direct_object.touch unless self.post_activity.parent.direct_object.blank?
     self.post_activity.parent.direct_activity_object.touch unless self.post_activity.parent.direct_activity_object.blank?
     self.post_activity.parent.touch
